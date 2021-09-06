02919d989a090ad1feae3dc28904de0064a8b1af
gr75801 <88337511+gr75801@users.noreply.github.com>
2021-09-06 09:27:20 +0530

    Add files via upload

---
+++ Admin Check.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host "Checking for elevated permissions..."
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+[Security.Principal.WindowsBuiltInRole] "Administrator")) {
+Write-Warning "Insufficient permissions to run this script. Open the PowerShell console as an administrator and run this script again."
+Break
+}
+else {
+Write-Host "Code is running as administrator — go on executing the script..." -ForegroundColor Green
+}
+
+
+([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(` [Security.Principal.WindowsBuiltInRole] "Administrator")
